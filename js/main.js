68ca91448b5c471381f1ab37e898ce0c71eab4db
Andrey Borzilo <64604386+Anrimad@users.noreply.github.com>
2020-06-14 01:43:22 +0300

    оживил форму на js

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var searchButton = document.querySelector('.search-button');
+var searchForm = document.querySelector('.search-form');
+var form = document.querySelector('.form');
+
+var dateArrival = document.querySelector('[name=date-arrival]');
+var dateLeaving = document.querySelector('[name=date-leaving]');
+var adult = document.querySelector('[name=adult]');
+var kids = document.querySelector('[name=kids]');
+
+var isStorageSupport = true;
+var storage = '';
+
+try {
+    storage = localStorage.getItem('dateArrival');
+} catch(err) {
+    isStorageSupport = false;
+}
+
+
+searchButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    searchForm.classList.add('shown');
+
+    if(storage) {
+        dateArrival.value = storage;
+        dateLeaving.focus();
+    } else {
+        dateArrival.focus();
+    }
+
+})
+
+form.addEventListener('submit', function(evt){ 
+    if(!dateArrival.value || !dateLeaving.value || !adult.value || !kids.value) {
+        evt.preventDefault();
+        form.classList.remove('form-error');
+        form.offsetWidth += 'some string';
+        console.log(form.offsetWidth);
+        
+        form.classList.add('form-error');
+    } else {
+        if(isStorageSupport) {
+            localStorage.setItem('dateArrival', dateArrival.value);
+        }
+    }
+
+});
+
+
+window.addEventListener('keydown', function(evt){
+    if(evt.keyCode === 27) {
+        if(searchForm.classList.contains('shown')) {
+            evt.preventDefault();
+            searchForm.classList.remove('shown');
+            form.classList.remove('form-error');
+        }
+    }
+});
